aa335ff1794439accbaa06f4f208aab7abec2728
umar <uhafesji@iut.univ-paris8.fr>
2025-03-06 14:33:18 +0100

    bd iut

--- utils/db_connection.py
+++ utils/db_connection.py
@@ -8,7 +8,6 @@ load_dotenv()
 
 DB_CONFIG = {
     "host": os.getenv("DB_HOST"),
-    "port": int(os.getenv("DB_PORT")),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
     "db": os.getenv("DB_NAME"),
